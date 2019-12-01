029bf397f488826fa8b051964796d17385de79e0
Karol P <karprzy7@gmail.com>
2019-12-01 23:28:19 +0100

    POC: new main with rtc enabled

---
+++ nrf51822/transmitter/src/main_rtc.c
@@ -0,0 +1,229 @@
+/**
+ * Copyright (c) 2014 - 2017, Nordic Semiconductor ASA
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ * 
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ * 
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+/** @file
+ * @defgroup rtc_example_main main.c
+ * @{
+ * @ingroup rtc_example
+ * @brief Real Time Counter Example Application main file.
+ *
+ * This file contains the source code for a sample application using the Real Time Counter (RTC).
+ *
+ */
+
+#include "nrf.h"
+#include "nrf_gpio.h"
+#include "nrf_drv_rtc.h"
+#include "nrf_drv_clock.h"
+#include "app_error.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#define COMPARE_COUNTERTIME  (300UL)                                        /**< Get Compare event COMPARE_TIME seconds after the counter starts from 0. */
+
+const nrf_drv_rtc_t rtc = NRF_DRV_RTC_INSTANCE(0); /**< Declaring an instance of nrf_drv_rtc for RTC0. */
+
+/**@brief Function for sending packet.
+ */
+void send_packet()
+{
+    // send the packet:
+    NRF_RADIO->EVENTS_READY = 0U;
+    NRF_RADIO->TASKS_TXEN   = 1;
+
+    while (NRF_RADIO->EVENTS_READY == 0U)
+    {
+        // wait
+    }
+    NRF_RADIO->EVENTS_END  = 0U;
+    NRF_RADIO->TASKS_START = 1U;
+
+    while (NRF_RADIO->EVENTS_END == 0U)
+    {
+        // wait
+    }
+
+    NRF_LOG_INFO("The packet was sent\r\n");
+
+    NRF_RADIO->EVENTS_DISABLED = 0U;
+    // Disable radio
+    NRF_RADIO->TASKS_DISABLE = 1U;
+
+    while (NRF_RADIO->EVENTS_DISABLED == 0U)
+    {
+        // wait
+    }
+}
+
+
+/**@brief Function for initialization oscillators.
+ */
+//TODO not needed here???? 
+void clock_initialization()
+{
+    /* Start 16 MHz crystal oscillator */
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_HFCLKSTART    = 1;
+
+    /* Wait for the external oscillator to start up */
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
+    {
+        // Do nothing.
+    }
+
+    /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
+    NRF_CLOCK->LFCLKSRC            = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_LFCLKSTART    = 1;
+
+    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
+    {
+        // Do nothing.
+    }
+}
+
+/*Start DHT22, measure RH and TEMP, return struct with that data */ 
+//TODO: implement struct which carries both RH and TEMP 
+static void dht_measure()
+{
+    DHT22_start ();
+    check_response ();
+    hum_int = read_data ();
+    hum_dec = read_data ();
+    temp_int = read_data ();
+    temp_dec = read_data ();
+    // checksum = read_data ();
+
+    /* Merge integral and decimal bytes together */
+    TEMP = ((temp_int<<8)|(temp_dec & 0xff));
+    RH = ((hum_int<<8)|(hum_dec & 0xff));
+
+    // TEMP = ROUNDED_DIV(TEMP, 10); //TODO make it divide using integers also test for negative
+    RH = ROUNDED_DIV(RH, 10);
+
+    temper = divide(TEMP, 10);
+
+
+
+
+    NRF_LOG_INFO("TEMP: %i.%i RH: %i\r\n", temper.quotient, temper.remainder, RH);
+}
+
+
+/** @brief: Function for handling the RTC0 interrupts.
+ * Triggered on TICK and COMPARE0 match.
+ */
+static void rtc_handler(nrf_drv_rtc_int_type_t int_type)
+{
+    if (int_type == NRF_DRV_RTC_INT_COMPARE0)
+    {
+        err_code = NRF_LOG_INIT(NULL);
+        APP_ERROR_CHECK(err_code);
+        NRF_LOG_INFO("Transmitter started\r\n");
+
+        radio_configure();
+        NRF_RADIO->PACKETPTR = (uint32_t)&packet;
+
+        dht_measure();
+
+        packet = TEMP; // here it should pass data from measurements! 
+        send_packet();
+        NRF_LOG_INFO("The contents of the package was %u\r\n", (unsigned int)packet);
+
+    }
+    else if (int_type == NRF_DRV_RTC_INT_TICK)
+    {
+        nrf_gpio_pin_toggle(TICK_EVENT_OUTPUT);
+    }
+}
+
+/** @brief Function starting the internal LFCLK XTAL oscillator.
+ */
+static void lfclk_config(void)
+{
+    ret_code_t err_code = nrf_drv_clock_init();
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_clock_lfclk_request(NULL);
+}
+
+/** @brief Function initialization and configuration of RTC driver instance.
+ */
+static void rtc_config(void)
+{
+    uint32_t err_code;
+
+    //Initialize RTC instance
+    nrf_drv_rtc_config_t config = NRF_DRV_RTC_DEFAULT_CONFIG;
+    config.prescaler = 4095;
+    err_code = nrf_drv_rtc_init(&rtc, &config, rtc_handler);
+    APP_ERROR_CHECK(err_code);
+
+    //Enable tick event & interrupt
+    nrf_drv_rtc_tick_enable(&rtc,true);
+
+    //Set compare channel to trigger interrupt after COMPARE_COUNTERTIME seconds
+    err_code = nrf_drv_rtc_cc_set(&rtc,0,COMPARE_COUNTERTIME * 8,true);
+    APP_ERROR_CHECK(err_code);
+
+    //Power on RTC instance
+    nrf_drv_rtc_enable(&rtc);
+}
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+
+    lfclk_config();
+
+    rtc_config();
+
+    while (true)
+    {
+        __SEV();
+        __WFE();
+        __WFE();
+    }
+}
+
+
+/**  @} */
