6274c0a378ff5f29b88a88a6a20e0dc97109f01a
Karol P <karprzy7@gmail.com>
2020-08-28 16:23:57 +0200

    Moved RPi specific contents to separate folder. Created webgui for outside sensor. Added reader note to guis Added entry to index.php

---
+++ rpi/index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Main page</h1>
+<form action="/cgi-bin/webgui.py" method="get">
+<input type="submit" value="Temperature">
+</form>
+<form action="/cgi-bin/ble_temp_webgui.py" method="get">
+<input type="submit" value="Outside (BLE)">
+</form>
+</body>
+</html>
